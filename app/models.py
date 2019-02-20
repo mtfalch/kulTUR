154acb98715019ec10265355d49bc1d416410de1
Marie Falch Orre <marief.orre@gmail.com>
2019-02-20 13:14:19 +0100

    added implementation for db

---
+++ app/models.py
@@ -0,0 +1,31 @@
+from app import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(64))
+    last_name = db.Column(db.String(64))
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    tlf = db.Column(db.Integer, unique=True)
+    password_hash = db.Column(db.String(128))
+    trips = db.relationship('UserTrips', backref='owner', lazy='dynamic')
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+class UserTrips(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.String(250))
+    time = db.Column(db.Date, index=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    route_id = db.Column(db.Integer, db.ForeignKey('route.id'))
+
+
+class Route(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    trips = db.relationship('UserTrips', backref='TripRoute', lazy='dynamic')
+
+
+
+
+
