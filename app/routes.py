6e4a325a008f9c18bc8192b1fb1dde45ad4af873
Marie Falch Orre <marief.orre@gmail.com>
2019-02-20 15:34:55 +0100

    fiks

--- app/routes.py
+++ app/routes.py
@@ -13,7 +13,7 @@ def signup():
     if form.validate_on_submit():
         flash('SignUp requested for user {}, remember_me={}'.format(
             form.username.data, form.remember_me.data))
-        return redirect(url_for('index'))
+        return redirect(url_for('mainpage'))
     return render_template('signup.html', title='Sign up', form=form)
 
 
