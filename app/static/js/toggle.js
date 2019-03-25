044cf2873d3e1f2c2cb45d9b8664b22879aafbb1
Marie Falch Orre <marief.orre@gmail.com>
2019-03-25 08:21:43 +0100

    moved track js to separat file2

--- app/static/js/toggle.js
+++ app/static/js/toggle.js
@@ -12,66 +12,6 @@ function doalert(checkboxElem) {
     }
 }
 
-function highlightFeature(e) {
-  var layer = e.target;
-
-  layer.setStyle({
-    //stroke: true,
-    weight: 10,
-    dashArray: '',
-    opacity: 0.7,
-    color: '#FFA061'
-
-  });
-
-  if (!L.Browser.ie && !L.Browser.opera) {
-    layer.bringToFront();
-  }
-}
-
-function resetHighlight(e) {
-  var layer = e.target;
-
-  layer.setStyle({
-      weight: 4,
-      color: '#FFA061',
-      dashArray: '',
-      opacity: 1
-  });
-}
-
-function onEachFeature(feature, layer) {
-    {
-        layer.bindPopup('<b>'+feature.properties.RUTENAVN+'</b><p> Turtype: '+feature.properties.OBJTYPE+'</p>');
-    }
-    layer.on({
-        mouseover: highlightFeature,
-        mouseout: resetHighlight,
-        //click: zoomToFeature
-    });
-}
-
-// ON/OFF tracks
-var tracks = L.geoJSON(geojson, {
-    onEachFeature: onEachFeature
-})
-
-tracks.setStyle({
-    weight: 4,
-    color: '#FFA061',
-    dashArray: '',
-    Opacity: 1
-})
-
-function toggletracks(checkboxElem) {
-    if (checkboxElem.checked) {
-        map.addLayer(tracks)
-
-    } else {
-        map.removeLayer(tracks);
-    }
-}
-
 function search(handleData){
     $.ajax({
         url: '/tracks/<location>',
