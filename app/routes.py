b1e7531ce7bcb3de204e0c8dc8072b0dc9194d5f
Marie Falch Orre <marief.orre@gmail.com>
2019-03-26 13:32:05 +0100

    fix

--- app/routes.py
+++ app/routes.py
@@ -5,6 +5,7 @@ from app import app, db
 from flask_login import current_user, login_user, logout_user, login_required
 from app.forms import SignUpForm, LoginForm, EditProfileForm, EditTlfForm, EditNameForm, EditEmailForm, EditSexForm, EditPasswordForm
 from app.models import User, Tracks
+from json import loads
 
 @app.route('/')
 @app.route('/index')
